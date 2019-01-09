a1ed60a622531ae99ff8c5844e0b6a1a42c62a61
Danilo Sato <dtsato@thoughtworks.com>
2019-01-09 11:47:34 +0000

    Allow to download model from Google Storage directly

--- src/download_data.py
+++ src/download_data.py
@@ -1,23 +1,31 @@
 import os
 from google.cloud import storage
+import argparse
 
-def load_data():
+def load_data(path, key):
     gcsBucket = "continuous-intelligence"
-    key = "store47-2016.csv"
 
-    if not os.path.exists('data/raw'):
-        os.makedirs('data/raw')
+    if not os.path.exists(path):
+        os.makedirs(path)
 
-    if not os.path.exists("data/raw/" + key):
+    if not os.path.exists(os.path.join(path, key)):
         client = storage.Client()
         bucket = client.get_bucket(gcsBucket)
         blob = bucket.get_blob(key)
-        blob.download_to_filename(os.path.join('data/raw', key))
+        blob.download_to_filename(os.path.join(path, key))
 
 
 def main():
-    print("Loading data...")
-    load_data()
+    parser = argparse.ArgumentParser(description='Download files from Google Storage.')
+    parser.add_argument('--model', action='store_true', default=False, help='Downloads model (data/decision_tree/model.pkl) instead of input file (data/raw/store47-2016.csv)')
+    args = parser.parse_args()
+
+    if args.model:
+        print("Loading model...")
+        load_data(path='data/decision_tree', key='model.pkl')
+    else:
+        print("Loading input data...")
+        load_data(path='data/raw', key='store47-2016.csv')
     print("Finished downloading")
 
 
